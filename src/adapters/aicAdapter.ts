aedd5549ade0f64f5380db7818a9b28d9c6b0874
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-10 14:50:30 +0100

    feat: add adapters for aic and met

---
+++ src/adapters/aicAdapter.ts
@@ -0,0 +1,25 @@
+import { AicBaseResponse, SingleAicArtworkResponse } from "@/types/aicTypes";
+import { Artwork } from "@/types/artworkType";
+
+export const aicAdapter = (
+  item: AicBaseResponse & Partial<SingleAicArtworkResponse["data"]>
+): Artwork => ({
+  id: item.id.toString(),
+  title: item.title,
+  artist: item.artist_display || undefined,
+  creationDate: item.date_display || undefined,
+  image: {
+    imageURL: `https://www.artic.edu/iiif/2/${item.image_id}/full/843,/0/default.jpg`,
+    altText: item.thumbnail?.alt_text ?? item.title,
+    thumbnail: item.thumbnail?.lqip,
+  },
+  medium: item.medium_display,
+  origin: item.place_of_origin,
+  styles:
+    item.style_titles || item.classification_title || item.department_title,
+  classification: item.artwork_type_title,
+  description: item.description,
+  exhibitionHistory: item.exhibition_history,
+  museumSource: "aic",
+  museumLink: `https://www.artic.edu/artworks/${item.id}`,
+});
