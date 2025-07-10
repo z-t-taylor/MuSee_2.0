fcbb1c7d9a85b3f36f81e56c289253dd3c5c5e31
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-10 14:48:29 +0100

    feat: add met types

---
+++ src/types/metTypes.ts
@@ -0,0 +1,20 @@
+export interface MetBaseResponse {
+  objectIDs: number[] | null;
+}
+
+export interface SingleMetArtworkResponse {
+  objectId: number;
+  isPublicDomain: boolean;
+  primaryImage: string;
+  primaryImageSmall: string;
+  title: string;
+  artistDisplayName: string;
+  artistDisplayBio: string;
+  objectDate: string;
+  artistNationality: string;
+  country?: string;
+  culture?: string;
+  medium: string;
+  classification: string;
+  objectURL: string;
+}
