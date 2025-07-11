42def70943fa5e791a6333be74c2075d859e534d
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-11 15:29:31 +0100

    feat: aic base controller logic

---
+++ src/app/api/aic/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+const AIC_BASE_URL = "https://api.artic.edu/api/v1/artworks";
+
+export async function GET() {
+  const res = await fetch(`${AIC_BASE_URL}?limit=10`);
+  const data = await res.json();
+  return NextResponse.json(data);
+}
