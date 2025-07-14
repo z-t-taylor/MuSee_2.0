30bb30e058c27ed3f0b21b042461e4d040b9e25f
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-14 13:54:24 +0100

    fix: change 'object' to 'objects' for correct met fetch

--- src/app/api/met/route.ts
+++ src/app/api/met/route.ts
@@ -4,10 +4,11 @@ const MET_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1";
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const id = url.searchParams.get("id");
+  console.log("id", id);
 
   if (!id) return NextResponse.json({ error: "Missing ID" }, { status: 400 });
 
-  const res = await fetch(`${MET_BASE_URL}/object/${id}`);
+  const res = await fetch(`${MET_BASE_URL}/objects/${id}`);
   const data = await res.json();
-  return NextResponse.json(data);
+  return NextResponse.json(data, { status: 200 });
 }
