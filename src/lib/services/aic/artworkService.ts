e90ae6c384136a9ad23b15b3abf504472e340cf2
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-17 16:52:11 +0100

    feat: split aic app logic into controller and service

---
+++ src/lib/services/aic/artworkService.ts
@@ -0,0 +1,24 @@
+import { aicAdapter } from "@/adapters/aicAdapter";
+import { AicArtworkListResponse } from "@/types/aicTypes";
+import { Artwork } from "@/types/artworkType";
+import { NextRequest, NextResponse } from "next/server";
+
+const AIC_BASE_URL = "https://api.artic.edu/api/v1/artworks";
+
+export async function fetchAicArtworks(
+  limit: number,
+  page: number
+): Promise<{
+  data: Artwork[];
+  pagination: AicArtworkListResponse["pagination"];
+}> {
+  const res = await fetch(`${AIC_BASE_URL}?limit=${limit}&page=${page}`);
+
+  const rawData: AicArtworkListResponse = await res.json();
+  const transformed = rawData.data.map(aicAdapter);
+
+  return {
+    data: transformed,
+    pagination: rawData.pagination,
+  };
+}
