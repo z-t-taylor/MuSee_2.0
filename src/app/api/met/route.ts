c03fb60999d0ffdb5a6c112ade357da2a89825b3
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2025-07-22 10:34:03 +0100

    feat: separate met app logic into controller and service

--- src/app/api/met/route.ts
+++ src/app/api/met/route.ts
@@ -1,18 +1,6 @@
-import { errorResponse } from "@/lib/utils/errorResponse";
-import { SingleMetArtworkResponse } from "@/types/metTypes";
-import { NextRequest, NextResponse } from "next/server";
-const MET_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1";
+import { getMetArtworks } from "@/lib/controllers/met/artworkController";
+import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const url = new URL(req.url);
-  const id = url.searchParams.get("id");
-
-  try {
-    const res = await fetch(`${MET_BASE_URL}/objects/${id}`);
-    if (!id) return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
-    const data: SingleMetArtworkResponse = await res.json();
-    return NextResponse.json(data, { status: 200 });
-  } catch (err) {
-    return errorResponse("Internal server error", 500);
-  }
+  return getMetArtworks(req);
 }
